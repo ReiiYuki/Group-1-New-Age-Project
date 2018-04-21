ec0d3f0dac5e070579c57e0162d6245b066467b4
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-22 03:50:09 +0700

    Taichi

---
+++ Assets/Scripts/TaichiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaichiController : MonoBehaviour {
+
+    public bool isPlay, isStop;
+    public string clipName;
+    
+        // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPlay)
+        {
+            GetComponent<Animation>().Play(clipName);
+            isPlay = false;
+        }
+        if (isStop)
+        {
+            GetComponent<Animation>().Stop();
+            isStop = false;
+        }
+	}
+}
